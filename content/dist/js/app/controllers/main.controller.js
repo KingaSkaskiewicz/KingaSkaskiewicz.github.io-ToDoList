f2137368917240a691d296a6316eec478ba36cba
Kinga <kinga123kon@gmail.com>
2020-04-02 12:36:57 +0200

    mój pierwszy commit

---
+++ content/dist/js/app/controllers/main.controller.js
@@ -0,0 +1,27 @@
+﻿app.controller('mainCtrl', function ($scope) {
+
+    $scope.model = {
+        tasks: [{
+            description: '',
+            isCompleted: false
+        }]
+    };
+
+
+    $scope.addNote = function () {
+        $scope.model.tasks.push({
+            description: '',
+            isCompleted: false
+        });
+    }
+
+    $scope.deleteNote = function (task) {
+        var index = $scope.model.tasks.indexOf(task);
+        if (index > -1) {
+            $scope.model.tasks.splice(index, 1);
+        }
+    }
+
+   
+
+});
